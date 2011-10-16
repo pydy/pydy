c75232397ee7ce39f8a8ec06c657ce4702c93eaa
Gilbert Gede <gilbertgede@gmail.com>
2011-10-16 14:40:30 -0700

    removed python template

--- double_pendulum/double_pendulum_integration.py
+++ double_pendulum/double_pendulum_integration.py
@@ -41,45 +41,4 @@ ax.set_ylabel('Angle, Angluar rate (rad, rad/s)')
 ax.legend(['q1', 'q2', 'u1', 'u2'])
 plt.show()
 
-"""
-# How to integrate equations of motion, quick and dirty way
-# get EoM into form <qdots, udots = expressions> first though
-# Also, make sure there are no qdots in rhs of udots
-# (meaning udot = f(q, u, t), not f(q, qdot, u, t)
-# use Kane.kindiffdict to get dictionary, and use subs on your udot vector to
-# get rid of qdots in bad places. See examples
 
-from numpy import array, linspace, sin, cos # etc.
-import matplotlib.pyplot as plt
-from scipy.integrate import odeint
-
-def rhs(y, t, arg1, arg2, etc.):
-    # unpack variables; remember the order here, it is important
-    var1, var2, etc. = y
-    # write return statement
-    # copy/paste qdots, udots from console/terminal
-    # make sure mechanics printing was turned on first though
-    return array([var1dot, var2dot, var3dot, etc.])
-
-# make tuple of arguement values (as floats or something)
-args = (arg1, arg2, etc.)
-# give initial conditions
-y0 = []
-# choose a timespan
-t = linspace(0, 1)
-# call odeint
-y = odeint(rhs, y0, t, args)
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(t, y)
-# give title to plot
-ax.set_title('Double Pendulum Example')
-# give x axis label
-ax.set_xlabel('Time (s)')
-# give y axis label
-ax.set_ylabel('Angle, Angluar rate (rad, rad/s)')
-# set legend values
-ax.legend(['var1', 'var2', 'var3', etc.])
-# show plot
-plt.show()
-"""
