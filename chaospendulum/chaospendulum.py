e58ec8f72ec96522be2b97bdfea035835499d635
Jason Moore <moorepants@gmail.com>
2011-10-16 17:34:56 -0700

    Completed the equations of motion for the chaos pendulum, but the indices option for reference frames seems to be buggy so it is not working.

--- chaospendulum/chaospendulum.py
+++ chaospendulum/chaospendulum.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python
+
 import sympy as sp
 import sympy.physics.mechanics as me
 
 # declare the coordinates and speeds and their derivatives #
 # theta : angle of the rod
 # phi : angle of the plate
-#!/usr/bin/env python
-
 # omega : angular speed of the rod
 # alpha : angular speed of the plate
 theta, phi, omega, alpha = me.dynamicsymbols('theta phi omega alpha')
@@ -15,22 +15,26 @@ thetad, phid, omegad, alphad = me.dynamicsymbols('theta phi omega alpha', 1)
 # gravity
 gravity = sp.symbols('g')
 # center of mass length, mass and  moment of inertia of the slender rod
-lA, mA, IA = sp.symbols('lA mA IA')
+lA, mA, IA11 = sp.symbols('lA mA IA11')
 # center of mass length, mass and moment of inertia of the plate
-lB, mB, IB1, IB2, IB3 = sp.symbols('lB mB IB1 IB2 IB3')
+lB, mB, IB11, IB22, IB33 = sp.symbols('lB mB IB11 IB22 IB33')
 
+# reference frames #
 # create a Newtonian reference frame
 N = me.ReferenceFrame('N', indices=['1', '2', '3'])
+# create a reference for the rod, A, and the plate, B
+A = me.ReferenceFrame('A', indices=['1', '2', '3'])
+B = me.ReferenceFrame('B', indices=['1', '2', '3'])
 
 # orientations #
 # the rod rotates with respect to the Newtonian reference frame about the 2
 # axis
-A = N.orientnew('A', 'Axis', [theta, N['2']])
+A.orient(N, 'Axis', [theta, N['2']])
 # the plate rotates about the rod's primay axis
-B = A.orientnew('B', 'Axis', [phi, N['3']])
+B.orient(A, 'Axis', [phi, N['3']])
 
 # positions #
-# original of the Newtonian reference frame
+# origin of the Newtonian reference frame
 No = me.Point('No')
 # create a point for the mass centers of the two bodies
 Ao = me.Point('Ao')
@@ -39,24 +43,62 @@ Bo = me.Point('Bo')
 Ao.set_pos(No, lA * A['3'])
 Bo.set_pos(No, lB * A['3'])
 
-# angular velocities and accelerations
+# angular velocities and accelerations #
 A.set_ang_vel(N, omega * N['2'])
 B.set_ang_vel(A, alpha * A['3'])
 
 A.set_ang_acc(N, A.ang_vel_in(N).dt(N))
 B.set_ang_acc(A, B.ang_vel_in(A).dt(A))
 
-# linear velocities and accelerations
-N.set_vel(N, 0)
-B.set_vel(N, omega * l * a.x)
-B.a2pt(N, n, b)
+# linear velocities and accelerations #
+No.set_vel(N, 0)
+Ao.set_vel(N, omega * lA * A['1'])
+Ao.a2pt_theory(No, N, A)
+Bo.set_vel(N, omega * lB * A['1'])
+Bo.a2pt_theory(No, N, A)
 
-I = inertia(b, I1, I2, I3)
+# kinematical differential equations #
+kinDiffs = [omega - thetad, alpha - phid]
 
-kd = [omega-thetad, alpha-phid]
-forcelist = [(B, -n.y*gravity*m)]
-plate = RigidBody()
-plate.mc = B
-plate.inertia = (I, B)
+# define the inertia dyadic for each body #
+IA = me.inertia(A, IA11, IA11, 0.0)
+IB = me.inertia(B, IB11, IB22, IB33)
+
+# rigid bodies #
+rod = me.RigidBody()
+rod.mc = Ao
+rod.inertia = (IA, Ao)
+rod.frame = A
+
+plate = me.RigidBody()
+plate.mc = Bo
+plate.inertia = (IB, Bo)
 plate.frame = B
-print b.dcm(n)
+
+bodyList = [rod, plate]
+
+# forces #
+# add the gravitional force to each body
+forceList = [(Ao, -N['3'] * gravity * mA), (Bo, -N['3'] * gravity * mB)]
+
+# equations of motion with Kane's method #
+# create a Kane object with respect to the Newtonian reference frame
+kane = me.Kane(N)
+# set the coordinates
+kane.coords([theta, phi])
+# set the speeds
+kane.speeds([omega, alpha])
+# set the kinematical differential equations
+kane.kindiffeq(kinDiffs)
+
+# calculate kane's equations
+kane.kanes_equations(forceList, bodyList)
+
+# solve the equations for the double dots
+massMatrix = kane.mass_matrix_full
+forcing = kane.forcing_full
+doubledots = massMatrix.inv() * forcing
+# substitute for the derivatives of the coordinates
+kinDiffDict = kane.kindiffdict()
+qudots = doubledots.subs(kinDiffDict)
+qudots.simplify()
