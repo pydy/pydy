6a2e0219cf574218fc8fd56b5d66bc4f432ab183
Dale Lukas Peterson <hazelnusse@gmail.com>
2011-10-24 19:10:23 -0700

    Renamed a few files and created a folder for the C++ example

--- double_pendulum/cpp/double_pendulum.cc
+++ double_pendulum/cpp/double_pendulum.cc
@@ -1,15 +1,15 @@
-#include "double_pendulum_ode.h"
+#include "double_pendulum.h"
 #include <cmath>
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_odeiv2.h>
 
-double double_pendulum_ke(double x[4], double params[3])
+double double_pendulum_ke(const double x[4], const double params[3])
 {
   double m = params[0], l = params[2];
   return 1.0*pow(l, 2)*(sin(x[0])*sin(x[1]) + cos(x[0])*cos(x[1]))*x[2]*x[3]/m + 1.0*pow(l, 2)*pow(x[2], 2)/m + 0.5*pow(l, 2)*pow(x[3], 2)/m;
 }
 
-double double_pendulum_pe(double x[4], double params[3])
+double double_pendulum_pe(const double x[4], const double params[3])
 {
   double m = params[0], g = params[1], l = params[2];
   return -g*m*(2*l*cos(x[0]) + l*cos(x[1]) - 3*l);
