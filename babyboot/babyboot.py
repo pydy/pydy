3017a2a3ec91ab75afd8ceac76ea3a913f2a194a
Christopher Dembia <cld72@cornell.edu>
2013-04-13 18:25:25 -0700

    Finished babyboot example.

--- babyboot/babyboot.py
+++ babyboot/babyboot.py
@@ -4,6 +4,7 @@ from sympy.physics.mechanics import *
 # -- Declare symbols.
 
 # Declare degrees of freedom, and their time derivatives.
+# TODO get rid of those which are unnecessary.
 qA, qB, uA, uB = dynamicsymbols('qA qB uA uB')
 qAd, qBd, uAd, uBd = dynamicsymbols('qA qB uA uB', 1)
 
@@ -18,33 +19,38 @@ mechanics_printing()
 # Fixed frame at base of upper rod.
 N = ReferenceFrame('N')
 # Frame tracking the upper rod.
-Aframe = N.orientnew('Aframe', 'Axis', [qA, N.x])
-Bframe = Aframe.orientnew('Bframe', 'Axis', [qB, Aframe.z])
+frameA = N.orientnew('frameA', 'Axis', [qA, N.x])
+frameB = frameA.orientnew('frameB', 'Axis', [qB, frameA.z])
 
 # TODO why do we need to do this?
-Aframe.set_ang_vel(N, qA * N.x)
-Aframe.set_ang_acc(N, Aframe.ang_vel_in(N).dt(Aframe)) # TODO
+frameA.set_ang_vel(N, uA * N.x)
+frameA.set_ang_acc(N, frameA.ang_vel_in(N).dt(N)) # TODO
+
+frameB.set_ang_vel(frameA, uB * frameA.z)
+frameB.set_ang_acc(frameA, frameB.ang_vel_in(frameA).dt(frameA))
 
 # Origin.
-O = Point('O')
-O.set_vel(N, 0)
+NO = Point('NO')
+NO.set_vel(N, 0)
 
 # Center of mass of upper rod.
-Acm = O.locatenew('Acm', - LA * Aframe.z)
-Acm.set_vel(N, LA * uA * Aframe.y)  # Don't need this in MotionGenesis.
-Bcm = O.locatenew('Bcm', - LB * Aframe.z)
-Bcm.set_vel(N, LB * uA * Aframe.y) # Don't need this in MotionGenesis.
+Acm = NO.locatenew('Acm', -LA * frameA.z)
+Acm.v2pt_theory(NO, N, frameA) # Don't need this in MotionGenesis.
+Bcm = NO.locatenew('Bcm', - LB * frameA.z)
+Bcm.v2pt_theory(NO, N, frameA) # Don't need this in MotionGenesis.
 
 # Inertia dyadic.
-IA = inertia(Aframe, IAx, 0, 0)
-IB = inertia(Bframe, IBx, IBy, IBz)
+# TODO inertia dyadics are about a specific point, right? are we requiring the user to define it about the COM always or something? idk if this info should be separate. we have inertia info floating about in 2 different locations.
+IA = inertia(frameA, IAx, 0, 0)
+IB = inertia(frameB, IBx, IBy, IBz)
 
 # Create rigid bodies.
-BodyA = RigidBody('BodyA', Acm, Aframe, mA, (IA, Acm))
-BodyB = RigidBody('BodyB', Bcm, Bframe, mB, (IB, Bcm))
+BodyA = RigidBody('BodyA', Acm, frameA, mA, (IA, Acm))
+BodyB = RigidBody('BodyB', Bcm, frameB, mB, (IB, Bcm))
 BodyList = [BodyA, BodyB]
 
 # Forces.
+# Would be nice to have a method that applies gravity force to all objects.
 ForceList = [(Acm, - mA * g * N.z), (Bcm, - mB * g * N.z)]
 
 # Kinematic differential equations. TODO necessary?
