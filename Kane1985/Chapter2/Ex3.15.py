8ed5485e347d8dde51670c1fdba96b254465a37b
Oliver Lee <oliverzlee@gmail.com>
2013-05-06 21:31:34 -0700

    Add comments to Ex3.15.py detailing problem.

--- Kane1985/Chapter2/Ex3.15.py
+++ Kane1985/Chapter2/Ex3.15.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Exercise 3.15 from Kane 1985"""
+"""Exercise 3.15 from Kane 1985
+A robot arm, composed of Rigid Bodies 'A', 'B', 'C', operates in Reference
+Frame E. 'A*', 'B*', 'C*' are Points marking the centers of mass for the Rigid
+Bodies 'A', 'B', 'C'.
+
+Rigid Body 'D' also is lies in Reference Frame 'E'. The center of mass of 'D'
+is marked as Point 'D*'. 'D' is fixed relative to 'C'.
+
+Each Reference Frame has a set of mutually perpendicular vectors x, y, z. 'A'
+is rotated by 'q0' relative to 'E' about an axis parallel to A.x. 'B' is
+rotated by 'q1' relative to 'A' about an axis parallel to A.y. Point 'P' is
+fixed in both 'A' and 'B'. A.x is parallel to E.x. A.y is parallel to B.y.
+
+Point 'O' is a point fixed in both 'E' and 'A'. 'A*' is separated from 'O' by
+'LA' * A.z. 'P' is separated from 'O' by 'LP' * A.z. 'B*' is separated from 'P'
+by 'LB' * B.z. 'C*' is separated from 'B*' by 'q2' * B.z. 'D*' is separated
+from 'C*' by 'p1'*B.x + 'p2'*B.y + 'p3'*B.z.
+
+We define:'q0d' = 'u1', 'q1d' = 'u2', 'q2d' = 'u3'.
+'LA', 'LB', 'LP', 'p1', 'p2', 'p3' are constants.
+"""
 
 from sympy.physics.mechanics import dynamicsymbols, MechanicsStrPrinter
 from sympy.physics.mechanics import ReferenceFrame, Point
@@ -10,6 +30,7 @@ def msprint(expr):
     pr = MechanicsStrPrinter()
     return pr.doprint(expr)
 
+# Define generalized coordinates, speeds, and constants:
 q0, q1, q2 = dynamicsymbols('q0 q1 q2')
 q0d, q1d, q2d = dynamicsymbols('q0 q1 q2', level=1)
 u1, u2, u3 = dynamicsymbols('u1 u2 u3')
@@ -17,20 +38,35 @@ LA, LB, LP = symbols('LA LB LP')
 p1, p2, p3 = symbols('p1 p2 p3')
 
 E = ReferenceFrame('E')
+# A.x of Rigid Body A is fixed in Reference Frame E and is rotated by q0.
 A = E.orientnew('A', 'Axis', [q0, E.x])
+# B.y of Rigid Body B is fixed in Reference Frame A and is rotated by q1.
 B = A.orientnew('B', 'Axis', [q1, A.y])
+# Reference Frame C has no rotation relative to Reference Frame B.
 C = B.orientnew('C', 'Axis', [0, B.x])
+# Reference Frame D has no rotation relative to Reference Frame C.
 D = C.orientnew('D', 'Axis', [0, C.x])
 
 pO = Point('O')
+# The vector from Point O to Point A*, the center of mass of A, is LA * A.z.
 pAs = pO.locatenew('A*', LA * A.z)
+# The vector from Point O to Point P, which lies on the axis where
+# B rotates about A, is LP * A.z.
 pP = pO.locatenew('P', LP * A.z)
+# The vector from Point P to Point B*, the center of mass of B, is LB * B.z.
 pBs = pP.locatenew('B*', LB * B.z)
+# The vector from Point B* to Point C*, the center of mass of C, is q2 * B.z.
 pCs = pBs.locatenew('C*', q2 * B.z)
+# The vector from Point C* to Point D*, the center of mass of D,
+# is p1 * B.x + p2 * B.y + p3 * B.z.
 pDs = pCs.locatenew('D*', p1 * B.x + p2 * B.y + p3 * B.z)
 
-A.set_ang_vel(E, u1 * A.x)
-B.set_ang_vel(A, u2 * B.y)
+# Define generalized speeds as:
+# u1 = q0d
+# u2 = q1d
+# u3 = q2d
+A.set_ang_vel(E, u1 * A.x) # A.x = E.x
+B.set_ang_vel(A, u2 * B.y) # B.y = A.y
 pCs.set_vel(B, u3 * B.z)
 
 pO.set_vel(E, 0) # Point O is fixed in Reference Frame E
@@ -41,6 +77,7 @@ pCs.v1pt_theory(pBs, E, B) # Point C* is moving in Reference Frame B
 pDs.set_vel(B, pCs.vel(B)) # Point D* is fixed relative to Point C* in B
 pDs.v1pt_theory(pBs, E, B) # Point D* is moving in Reference Frame B
 
+# Write generalized speeds as kinematic equations:
 kinematic_eqs = []
 kinematic_eqs.append(u1 - q0d)
 kinematic_eqs.append(u2 - q1d)
