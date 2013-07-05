5bbb48031450791cae27a35883b7fc8dbd89ba31
Tarun Gaba <tarun.gaba7@gmail.com>
2013-07-04 19:49:30 -0700

    Added test for cone Shape, initialized a new file for writing tests for VisualizationFrame.

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -1,37 +1,75 @@
 from sympy.physics.mechanics import *
-#from pydy-viz import Shape,Cube
+#from pydy-viz import Shape, Cube, 
 
 def test_shape():
     point_list = [(1, 0, 0), (2, 3, 1), (-1, 5, -1), (-4, 2, 5)]
-    shape = Shape('shape', pt_list=point_list, color="RED", \
-                                            origin=(0, 0, 0))
-    assert shape.get_color() == 'RED'
+    shape = Shape('shape')
+    
+    #checking setter methods ..
+    shape.set_point_list(point_list)
+    shape.set_color('RED')  #abstract method (abc package)
+    shape.set_center([0, 0, 0]) #abstract method
+    
+    #checking getter methods ...
+    assert shape.get_name() == 'shape' #abstract method
+    
     #How to compare lists?
     assert shape.get_point_list() ==  [(1, 0, 0), (2, 3, 1), \
                                       (-1, 5, -1), (-4, 2, 5)]
-    assert shape.get_name() == 'shape'
+    
+    assert shape.get_color() == 'RED' #abstract method
     #Again how to compare tuples
-    assert shape.get_origin() == (0, 0, 0)
+    assert shape.get_center() == [0, 0, 0]  #abstractmethod
 
 
 def test_cube():
-    cube = Cube('cube', length=10, color="BLUE", origin=(0,0,0))
-    assert cube.color() == 'BLUE'
+    cube = Cube('cube')
+    cube.set_length(10)
+    cube.set_color('BLUE')
+    cube.set_center([0, 0, 0])
+    
+    assert cube.get_name() == 'Cube'
+    assert cube.get_length() == 10
     assert cube.get_point_list() ==  [(5, 5, 5), (5, 5, -5), \
                                      (5, -5, 5), (-5, -5, -5), \
                                        (-5 ,5 , 5), (-5, 5, -5), \
                                           (-5, -5, 5), (-5, -5, -5)]
-    assert cube.get_name() == 'Cube'
-    assert cube.get_origin == (0, 0, 0)
+    assert cube.color() == 'BLUE'
+    assert cube.get_center() == [0, 0, 0]
+    
     assert isinstance(cube, Shape) == True
 
 def test_cylinder():
-    cylinder = Cylinder('cylinder',length=10,radius=5,color="BLACK",\
-                                         origin=(0,0,0))
-    assert cylinder.color() == 'BLACK'
+    cylinder = Cylinder('cylinder')
+    cylinder.set_height(10)
+    cylinder.set_radius(5)
+    cylinder.set_color('BLACK')
+    cylinder.set_center([0, 0, 0])
+    
+    
     assert cylinder.get_name() == 'cylinder'
-    assert cylinder.get_origin() == (0, 0, 0)
+    assert cylinder.get_height() == 10
+    assert cylinder.get_radius() == 5
+    assert cylinder.color() == 'BLACK'
+    assert cylinder.get_center() == (0, 0, 0)
+    
     assert isinstance(cylinder,Shape) == True
-                                                       
+    
+def test_cone():
+    cone = Cone('cone')
+    cone.set_height(10)
+    cone.set_radius(5)
+    cone.set_color('GREEN')
+    cone.set_center([0, 0, 0])	 #base center, or geometric center?
+    
+    assert cone.get_name() == 'cone'
+    assert cone.get_height() == 10
+    assert cone.get_radius() == 5
+    assert cone.get_color() == 'GREEN'                                            
+    assert cone.get_center() == [0, 0, 0]
+    
+    assert isinstance(cone,Shape) == True
+                            
+	                                                       
  
 
---
+++ tests/test_vframe.py
@@ -0,0 +1,4 @@
+#from pydy-viz import VisualizationFrame
+
+def test_visualizattion_frame():
+	frame1 = VisualizationFrame('frame1')
