c2ab12c36b601c876d4d534e59e62cc88e2c4c08
Tarun Gaba <Tarun Gaba>
2013-07-09 11:26:40 +0530

    Added colors as per the matplotlib's color module. Colors would be defined as strings of some keywords, or html hex codes, which would then be converted to rgba vals by using matplotlib's ColorConverter

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -26,11 +26,11 @@ def test_cube():
 
 def test_cylinder():
     cylinder = Cylinder('cylinder', height=10, radius=5, \
-                                    color='BLACK', center=[0,0,0])
+                                    color='blue', center=[0,0,0])
     assert cylinder.name == 'cylinder'
     assert cylinder.height == 10
     assert cylinder.radius == 5
-    assert cylinder.color == 'BLACK'
+    assert cylinder.color == 'blue'
     assert_allclose(cylinder.center, [0, 0, 0])
     
     
@@ -43,8 +43,8 @@ def test_cylinder():
     cylinder.radius = 7
     assert cylinder.radius == 7
 
-    cylinder.color = 'PINK'
-    assert cylinder.color == 'PINK'
+    cylinder.color = 'cyan'
+    assert cylinder.color == 'cyan'
 
     cylinder.center = [3, 6, -2]
     assert_allclose(cylinder.center, [3, 6, -2])
@@ -53,11 +53,11 @@ def test_cylinder():
     
 def test_cone():
     cone = Cone('cone',heigth=10, radius=5, \
-                        color='GREEN', center=[0,0,0])
+                        color='darkblue', center=[0,0,0])
     assert cone.name == 'cone'
     assert cone.height == 10
     assert cone.radius == 5
-    assert cone.color == 'GREEN'
+    assert cone.color == 'darkblue'
     assert_allclose(cone.center, [0, 0, 0])
     
     cone.name = 'cone1'
@@ -69,8 +69,8 @@ def test_cone():
     cone.radius = 3
     assert cone.radius == 3
 
-    cone.color = 'BLUE'
-    assert cone.color == 'BLUE'                                            
+    cone.color = 'darkcyan'
+    assert cone.color == 'darkcyan'                                            
     
     cone.center = [3, 7, 3]
     assert_allclose(cone.center, [3, 7, 3])
@@ -78,11 +78,11 @@ def test_cone():
     assert isinstance(cone, Shape)
                             
 def test_sphere():
-    sphere = Sphere('sphere', radius=10, color='GREY', \
+    sphere = Sphere('sphere', radius=10, color='azure', \
                                    center=[0,0,0])
     assert sphere.name == 'sphere1'
     assert sphere.radius == 10
-    assert sphere.color == 'GREY'
+    assert sphere.color == 'azure'
     assert_allclose(sphere.center, [0, 0, 0])	                                                       
     sphere.name = 'sphere1'
     assert sphere.name == 'sphere1'
@@ -90,8 +90,8 @@ def test_sphere():
     sphere.radius = 14
     assert sphere.radius == 14
 
-    sphere.color = 'GREEN'
-    assert sphere.color == 'GREEN'
+    sphere.color = 'aqua'
+    assert sphere.color == 'aqua'
 
     sphere.center = [5, 2, 6]
     assert_allclose(sphere.center, [5, 2, 6])
@@ -102,11 +102,11 @@ def test_sphere():
 
 def test_circle():
     circle = Circle('circle', radius=10, \
-                           color='CYAN',center=[0,0,0])
+                           color='gold',center=[0,0,0])
 
     assert circle.name == 'circle'
     assert circle.radius == 10
-    assert circle.color == 'CYAN'
+    assert circle.color == 'gold'
     assert_allclose(circle.center, [0, 0, 0])
 
     circle.name = 'circle1'
@@ -115,8 +115,8 @@ def test_circle():
     circle.radius = 12
     assert circle.radius == 12
 
-    circle.color = 'GREEN'
-    assert circle.color == 'GREEN'
+    circle.color = 'black'
+    assert circle.color == 'black'
 
     circle.center = [0, -3, 5]
     assert_allclose(circle.center, [0, -3, 5])
@@ -129,10 +129,10 @@ def test_mesh_shape():
                          [2, 8, 4], [7, 4, 1]]
     
     mesh_shape = MeshShape('mesh_shape', points=point_list, \
-                               color='PINK', origin=[0,0,0])                     
+                               color='green', origin=[0,0,0])                     
     assert mesh_shape.name == 'mesh_shape'
     assert_allclose(mesh_shape.points,point_list)
-    assert mesh_shape.color == 'PINK'
+    assert mesh_shape.color == 'green'
     assert_allclose(mesh_shape.origin, point_list)
     
     mesh_shape.name = 'mesh_shape1'
@@ -142,8 +142,8 @@ def test_mesh_shape():
     mesh_shape.points = new_point_list
     assert_allclose(mesh_shape.points, new_point_list)
     
-    mesh_shape.color = 'BLACK'
-    assert mesh_shape.color == 'BLACK'
+    mesh_shape.color = 'pink'
+    assert mesh_shape.color == 'pink'
     
     mesh_shape.origin = [-5, 1, 2]
     assert_allclose(mesh_shape.origin, [-5, 1, 2])
@@ -152,11 +152,11 @@ def test_mesh_shape():
 
 def test_plane():
     plane = Plane('plane', length=10, width=20, \
-                       color='BLUE', center=[0,0,0])
+                       color='indigo', center=[0,0,0])
     assert plane.name == 'plane'
     assert plane.length == 10
     assert plane.width == 20
-    assert plane.color == 'BLUE'
+    assert plane.color == 'indigo'
     assert_allclose(plane.center, [0, 0, 0])
     
     plane.name = 'plane1'
@@ -168,8 +168,8 @@ def test_plane():
     plane.width = 10
     assert plane.width == 10
     
-    plane.color = 'BLACK'
-    assert plane.color == 'BLACK'
+    plane.color = 'lavender'
+    assert plane.color == 'lavender'
     
     plane.center = [7, 4, 5]
     assert_allclose(plane.center, [7, 4, 5])
@@ -182,10 +182,10 @@ def test_tetrahedron():
     #circumscribed sphere. It would be mentioned explicitly in the
     #docstrings
     tetrahedron = Tetrahedron('tetrahedron', radius=5, \
-                                    color='BLACK', center=[0,0,0])
+                                    color='maroon', center=[0,0,0])
     assert tetrahedron.name == 'tetrahedron'
     assert tetrahedron.radius == 5
-    assert tetrahedron.color == 'BLACK'
+    assert tetrahedron.color == 'maroon'
     assert_allclose(tetrahedron.center, [0, 0, 0])
     
     
@@ -195,8 +195,8 @@ def test_tetrahedron():
     tetrahedron.radius = 7
     assert tetrahedron.radius == 7
 
-    tetrahedron.color = 'BLUE'
-    assert tetrahedron.color == 'BLUE'
+    tetrahedron.color = 'orange'
+    assert tetrahedron.color == 'orange'
 
     tetrahedron.center = [0, 1, -4]
     assert_allclose(tetrahedron.center, [0, 1, -4])
@@ -205,10 +205,10 @@ def test_tetrahedron():
 
 def test_octahedron():
     octahedron = Octahedron('octahedron', radius=12, \
-                                    color='BLACK', center=[0,0,0])
+                                    color='purple', center=[0,0,0])
     assert octahedron.name == 'octahedron'
     assert octahedron.radius == 12
-    assert octahedron.color == 'RED'
+    assert octahedron.color == 'purple'
     assert_allclose(octahedron.center, [0, 0, 0])
     
     
@@ -218,8 +218,8 @@ def test_octahedron():
     octahedron.radius = 2
     assert octahedron.radius == 2
 
-    octahedron.color = 'YELLOW'
-    assert octahedron.color == 'YELLOW'
+    octahedron.color = 'red'
+    assert octahedron.color == 'red'
 
     octahedron.center = [21, 11, 21]
     assert_allclose(octahedron.center, [21, 11, 21])
@@ -228,10 +228,10 @@ def test_octahedron():
 
 def test_icosahedron():
     icosahedron = Icosahedron('icosahedron', radius=11, \
-                                    color='BLUE', center=[0,0,0])
+                                    color='#FDF5E6', center=[0,0,0])
     assert icosahedron.name == 'icosahedron'
     assert icosahedron.radius == 11
-    assert icosahedron.color == 'BLUE'
+    assert icosahedron.color == '#FDF5E6'
     assert_allclose(icosahedron.center, [0, 0, 0])
     
     
@@ -241,8 +241,8 @@ def test_icosahedron():
     icosahedron.radius = 3
     assert icosahedron.radius == 3
 
-    icosahedron.color = 'PINK'
-    assert icosahedron.color == 'PINK'
+    icosahedron.color = '#FFC0CB'
+    assert icosahedron.color == '#FFC0CB'
 
     icosahedron.center = [10, 11, 12]
     assert_allclose(icosahedron.center, [10, 11, 12])
@@ -251,12 +251,12 @@ def test_icosahedron():
 
 def test_torus():
     torus = Torus('torus', radius=10, tube_radius=2, \
-                        color='BLUE', center=[0,0,0])
+                        color='#FFFF00', center=[0,0,0])
 
     assert torus.name == 'torus' 			
     assert torus.radius == 10
     assert torus.tube_radius == 2
-    assert torus.color == 'BLUE'
+    assert torus.color == '#FFFF00'
     assert_allclose(torus.center, [0,0,0]) 
 
     torus.name = 'torus1'
@@ -268,8 +268,8 @@ def test_torus():
     torus.tube_radius = 4
     assert torus.tube_radius == 4
     
-    torus.color = 'RED'
-    assert torus.color == 'RED'
+    torus.color = '#FFFFFF'
+    assert torus.color == '#FFFFFF'
 
     torus.center = [4. 7. 9]
     assert_allclose(torus.center, [4, 7, 9])
@@ -279,12 +279,12 @@ def test_torus():
 def test_tube():
     point_list = [[2, 4, 5], [2, 6, 4], [1, 5, 8]]
     tube = Tube('tube', radius=10, points=point_list, \
-                        color='RED', origin=[0,0,0])
+                        color='#4682B4', origin=[0,0,0])
 
     assert tube.name == 'tube' 			
     assert tube.radius == 10
     assert_allclose(tube.points, point_list)
-    assert tube.color == 'RED'
+    assert tube.color == '#4682B4'
     assert_allclose(tube.origin, [0,0,0]) 
 
     tube.name = 'tube1'
@@ -297,8 +297,8 @@ def test_tube():
     tube.points = new_point_list
     assert_allclose(tube.points, new_point_list)
     
-    tube.color = 'PINK'
-    assert tube.color == 'PINK'
+    tube.color = 'pink'
+    assert tube.color == 'pink'
     
     tube.origin = [4. 7. 9]
     assert_allclose(tube.origin, [4, 7, 9])
@@ -308,12 +308,12 @@ def test_tube():
 def test_torus_knot():
     
     torus_knot = TorusKnot('torus_knot', radius=10, tube_radius=2, \
-                        color='PINK', center=[0,0,0])
+                        color='#C0C0C0', center=[0,0,0])
 
     assert torus_knot.name == 'torus_knot' 			
     assert torus_knot.radius == 10
     assert torus_knot.tube_radius == 2
-    assert torus_knot.color == 'BLUE'
+    assert torus_knot.color == '#C0C0C0'
     assert_allclose(torus_knot.center, [0,0,0]) 
 
     torus_knot.name = 'torus_knot1'
@@ -325,8 +325,8 @@ def test_torus_knot():
     torus_knot.tube_radius = 1
     assert torus_knot.tube_radius == 1
     
-    torus_knot.color = 'BLACK'
-    assert torus_knot.color == 'BLACK'
+    torus_knot.color = '#2E8B57'
+    assert torus_knot.color == '#2E8B57'
 
     torus_knot.center = [1. 2. 1]
     assert_allclose(torus_knot.center, [1, 2, 1])
