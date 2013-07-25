0a37d5a8dfd0ba2fc8dd9f103c20db9a4ed43762
Tarun Gaba <Tarun Gaba>
2013-07-26 02:19:48 +0530

    Added generate_dict method for Shape class

--- pydy_viz/shapes.py
+++ pydy_viz/shapes.py
@@ -16,6 +16,9 @@ class Shape(object):
     A Shape. It is a superclass for more general shapes like
     Mesh, Cylinder, Sphere etc. 
     
+    Default Shape can be used for Particle visualizations, 
+    as in sympy.physics.mechanics.particle
+    
     The Shape classes are used for creating visualization objects, used
     for studying multibody dynamics and in animations.
     
@@ -36,9 +39,9 @@ class Shape(object):
     >>> from pydy_viz.shapes import Shape
     >>> 
     >>> s = Shape()
-    >>> s.name()
+    >>> s.name
     'UnNamed'
-    >>> s.color()
+    >>> s.color
     'grey'
     >>> s.color_in_rgb()
     (0.5019607843137255, 0.5019607843137255, 0.5019607843137255)
@@ -47,12 +50,12 @@ class Shape(object):
     >>> s.name
     'my-shape1'
     >>> s.color = 'blue'
-    >>> s.color()
+    >>> s.color
     'blue'
     >>> a = Shape('my-shape2', 'red')
-    >>> a.name()
+    >>> a.name
     'my-shape2'
-    >>> a.color()
+    >>> a.color
     'red'
     >>> a.color_in_rgb()
     (1.0, 0.0, 0.0)
@@ -113,74 +116,26 @@ class Shape(object):
         """
         return self._color_rgb
 
-    def _generate_dict(self):
+    def generate_dict(self):
         """
         Generates data dict along with the Shape info
         to be used by VisualizationFrame class.
         """
+        self._data_dict = {}
+        self._data_dict['name'] = self._name
+        self._data_dict['color'] = self._color_rgb
+        self._data_dict['type'] = self.__repr__()
         
     def __str__(self):
         return 'Shape ' + self._name + ' color:' + self._color
     
     def __repr__(self):
         return 'Shape'    
-
-    def generate_visualization_frame(self, reference_frame, origin, \
-                                                      name='UnNamed'):
-        ## I am not sure whether we want to include this method.
-        ## Or we want to generate VisualizationFrames from Scene class 
-        #Instead. 
         
-        """
-        creates a VisualizationFrame with origin as the center
-        of this shape.
-        
-        Parameters
-        ==========
-        reference_frame: an Instance of mechanics.ReferenceFrame
-        The frame of reference the VisualizationFrame is oriented in.
-        origin: an Instance of mechanics.Point
-        The origin point of the VisualizationFrame
-        
-        Examples
-        ========
-
-        >>> from pydy_viz import Shape
-        >>> from sympy.physics.mechanics import ReferenceFrame, Point
-        >>> s = Shape()
-        >>> I = ReferenceFrame('I')
-        >>> O = Point('O')
-        >>> f = s.generate_visualization_frame(I,O,name='frame1')
-        
-        """
-        ##Writing the general concept for initializing a visualization
-        #frame. It might need to change with the addition of 
-        #VisualizationFrame class
-        
-        if not isinstance(name, str):
-            raise TypeError('name should be a valid str object')
-        else:
-            _frame = VisualizationFrame(name)
-        
-            
-        if not isinstance(reference_frame, ReferenceFrame):
-            raise TypeError('%s should be a valid \
-                                     ReferenceFrame'%reference_frame)        
-        else:
-            _frame.reference_frame = reference_frame                             
-
-        if not isinstance(origin, Point):
-            raise TypeError('%s should be a valid \
-                                     Point'%origin)        
-        else:
-            _frame.origin = origin       
-        
-        return _frame
-            
 class Cube(object):
     """
     A Cube. This class generates a Cube, with given length of side, 
-    and color.Default color is Grey.
+    and color.Default color is grey.
     
     Parameters
     ==========
