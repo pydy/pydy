e0fff9f7abd38e018cdcfaa50fda7748ca38168f
Tarun Gaba <Tarun Gaba>
2013-07-28 16:21:49 +0530

    Modified some VisualizationFrame tests

--- pydy_viz/tests/test_visualization_frame_scene.py
+++ pydy_viz/tests/test_visualization_frame_scene.py
@@ -1,6 +1,5 @@
 from sympy.physics.mechanics import *
 from sympy import sin, cos, symbols
-#from pydy-viz import *
 
 class TestVisualizationFrameScene(object):
     
@@ -24,41 +23,43 @@ class TestVisualizationFrameScene(object):
         self.point_list1 = [[2, 3, 1], [4, 6, 2], [5, 3, 1], [5, 3, 6]]
         self.point_list2 = [[3, 1, 4], [3, 8, 2], [2, 1, 6], [2, 1, 1]]
 
-        self.mesh_shape1 = MeshShape('mesh_shape1', \
-                                points=self.point_list1, color='blue')    
-        self.mesh_shape2 = MeshShape('mesh_shape2', \
-                                points=self.point_list2, color='red')     
+        self.mesh_shape1 = Cylinder()
+        self.mesh_shape2 = Cylinder()
                                
                                
         self.Ixx, self.Iyy, self.Izz = symbols('Ixx Iyy Izz')
         self.mass = symbols('mass')
-        self.parameters = [self.Ixx, self.Iyy, self.Ixzz, self.mass]
+        self.parameters = [self.Ixx, self.Iyy, self.Izz, self.mass]
         self.param_vals = [0, 0, 0, 0] 
         
         self.inertia = inertia(self.A, self.Ixx, self.Iyy, self.Izz)
 
         self.rigid_body = RigidBody('rigid_body1', self.P1, self.A, \
                                  self.mass, (self.inertia, self.P1))
-                          
-
+        
+        self.global_frame1 = VisualizationFrame('global_frame1', \
+                                self.A, self.P1, shape=self.mesh_shape1)                  
+                                 
+        self.global_frame2 = VisualizationFrame('global_frame2', \
+                                self.B, self.P2, shape=self.mesh_shape2)                  
 
         self.particle = Particle('particle1', self.P1, self.mass)                            
-
-        self.transformation_matrix = [[cos(self.p[1])*cos(self.p[2]),  \
-                  sin(self.p[0])*sin(self.p[1])*cos(self.p[2]) + \
-                                     sin(self.p[2])*cos(self.p[0]), \
-                  sin(self.p[0])*sin(self.p[2]) - \
-                        sin(self.p[1])*cos(self.p[0])*cos(self.p[2]), \
-                  10], [-sin(self.p[2])*cos(self.p[1]), \
-                     -sin(self.p[0])*sin(self.p[1])*sin(self.p[2]) + \
-                             cos(self.p[0])*cos(self.p[2]), \
-                             sin(self.p[0])*cos(self.p[2]) + \
-                     sin(self.p[1])*sin(self.p[2])*cos(self.p[0]),10], \
-                    [sin(self.p[1]), -sin(self.p[0])*cos(self.p[1]), \
-                                   cos(self.p[0])*cos(self.p[1]),10], \
-                                     [0, 0, 0, 1]]
+        
+        #To make it more readable      
+        p = self.p
+        q = self.q 
+        #Here is the dragon ..
+        self.transformation_matrix = \
+            [[cos(p[1])*cos(p[2]), sin(p[2])*cos(p[1]), -sin(p[1]), 0], \
+             [sin(p[0])*sin(p[1])*cos(p[2]) - sin(p[2])*cos(p[0]), \
+                  sin(p[0])*sin(p[1])*sin(p[2]) + cos(p[0])*cos(p[2]), \
+                  sin(p[0])*cos(p[1]), 0], \
+             [sin(p[0])*sin(p[2]) + sin(p[1])*cos(p[0])*cos(p[2]), \
+                 -sin(p[0])*cos(p[2]) + sin(p[1])*sin(p[2])*cos(p[0]), \
+                  cos(p[0])*cos(p[1]), 0], \
+             [10, 10, 10, 1]]
     
-
+    
     def test_vframe_with_rframe(self):
         self.frame1 = VisualizationFrame('frame1', self.I, self.O, \
                                                 shape=self.mesh_shape1)
@@ -85,7 +86,6 @@ class TestVisualizationFrameScene(object):
 
 
     def test_vframe_with_rbody(self):
-    
         self.frame2 = VisualizationFrame('frame2', self.rigid_body, \
                                                 shape=self.mesh_shape1)
         
@@ -145,7 +145,7 @@ class TestVisualizationFrameScene(object):
         self.frame4 = VisualizationFrame(self.I, self.O, \
                                                shape=self.mesh_shape1)
         
-        assert self.frame4.name == 'unnamed'
+        assert self.frame4.name == 'UnNamed'
         #To check if referenceframe and origin are defined 
         #properly without name arg
         assert self.frame4.reference_frame == self.I 
@@ -159,14 +159,19 @@ class TestVisualizationFrameScene(object):
         self.frame5 = VisualizationFrame('parent-frame', self.I, \
                                         self.O, shape=self.mesh_shape1)
         
-        self.frame5.add_child_frames(self.frame1, self.frame2, \
-                                                       self.frame3)                                                        
-        assert self.frame5.get_children()[0] is self.frame1
-        assert self.frame5.get_children()[1] is self.frame2
-        assert self.frame5.get_children()[2] is self.frame3
+        self.frame5.add_child_frames(self.global_frame1, \
+                                          self.global_frame2)
+                                                                                                            
+        assert self.frame5.child_frames[0] is self.global_frame1
+        assert self.frame5.child_frames[1] is self.global_frame2
+        
+        self.frame5.remove_child_frames(self.global_frame1, \
+                                                  self.global_frame2)
+        
+        assert len(self.frame5.child_frames) == 0                                           
 
     def test_numeric_transform(self):
-        self.list2 = [[1.0, 0.0, 0.0, 0.0], \
+        self.list1 = [[1.0, 0.0, 0.0, 0.0], \
                       [0.0, 1.0, 0.0, 0.0], \
                       [0.0, 0.0, 1.0, 0.0], \
                       [10.0, 10.0, 10.0, 1.0]]
@@ -177,22 +182,19 @@ class TestVisualizationFrameScene(object):
                       [20.0, 20.0, 20.0, 1.0]]
                       
 
-        self.frame1.transform(self.I, self.O)
-        self.frame1.generate_numeric_transform(self.dynamic, \
+        self.global_frame1.transform(self.I, self.O)
+        self.global_frame1.generate_numeric_transform(self.dynamic, \
                                                         self.parameters)
         
-        assert self.frame1.evaluate_numeric_transform(self.states, \
+        assert self.global_frame1.evaluate_numeric_transform(self.states, \
                                        self.param_vals).tolist() == \
                                        self.list1
         
-        self.frame1.reference_frame = self.B
-        self.frame1.origin = self.P2                                         
-        
-        self.frame1.transform(self.I, self.O)
-        self.frame1.generate_numeric_transform(self.dynamic, \
+        self.global_frame2.transform(self.I, self.O)
+        self.global_frame2.generate_numeric_transform(self.dynamic, \
                                                         self.parameters)
         
-        assert self.frame1.evaluate_numeric_transform(self.states, \
+        assert self.global_frame2.evaluate_numeric_transform(self.states, \
                                        self.param_vals).tolist() == \
                                        self.list2
         
@@ -219,9 +221,9 @@ class TestVisualizationFrameScene(object):
         #We can check transformation matrix for camera, in the extended 
         #example.
         
-        #unnamed camera
+        #UnNamed camera
         camera1 = Camera(self.I, self.O)
-        assert camera1.name == 'unnamed'
+        assert camera1.name == 'UnNamed'
         assert camera1.reference_frame == self.I
         assert camera1.origin == self.O
 
