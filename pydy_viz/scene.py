e98a96e689ee5840f6e12873ba7e6f5c5317aa7f
Tarun Gaba <Tarun Gaba>
2013-08-01 21:46:49 +0530

    WIP Scene class

--- pydy_viz/scene.py
+++ pydy_viz/scene.py
@@ -1,4 +1,4 @@
-
+#TODO imports
 
 class Scene():
     """
@@ -70,20 +70,81 @@ class Scene():
         else:
             self.lights = lights    
         
+        #Do we need additional methods for adding_vframes?
+        #Or just use lists append and remove methods?
+        
         self.visualization_frames = visualization_frames
         self.width = width
         self.height = height
         
         @property
         def name(self):
+            """
+            Name of Scene.
+            """
             return self._name
             
-        @name.setter    
+        @name.setter
         def name(self, new_name):
             if not isinstance(new_name, str):
                 raise TypeError('Name should be a valid str.')
             else:
                 self._name = new_name
         
-                    
+           @property
+    def origin(self):
+        """
+        origin of the Scene.
+        
+        """
+        return self._origin
+        
+    @origin.setter
+    def origin(self, new_origin):
+        if not isinstance(new_origin, Point):
+            raise TypeError('''origin should be a valid Point Object''')
+        else:
+            self._origin = new_origin        
+            
+    @property
+    def reference_frame(self):
+        """
+        reference_frame of the Scene.
+        
+        """
+        return self._reference_frame
+        
+    @reference_frame.setter
+    def reference_frame(self, new_reference_frame):
+        if not isinstance(new_reference_frame, ReferenceFrame):
+            raise TypeError('''reference_frame should be a valid
+                                ReferenceFrame object.''')
+        else:
+            self._reference_frame = new_reference_frame             
     
+    def generate_json(self, state_sym, par_sym, states, parameters, \
+                                              save_to='data.json'):
+        """
+        TODO: Docstring
+        """
+        out_file = save_to
+        self._scene_data = {}
+        self._scene_data['name'] = self._name
+        self._scene_data['height'] = self._height
+        self._scene_data['width'] = self._width
+        self._scene_data['frames'] = []
+
+        for frame in self.visualization_frames:
+
+            frame.transform(self._reference_frame, self._origin)
+            frame.generate_numeric_transform(state_sym, par_sym)
+            frame.evaluate_numeric_transform(states, parameters)
+            self._scene_data['frames'].append(frame.generate_simulation_dict())
+
+        out_file = open()
+        #return self._scene_data
+        
+
+    def display(self, json=None):
+        #TODO
+        
