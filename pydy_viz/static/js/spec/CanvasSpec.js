09577aa5bf9b312440e12f71d1210e7792ffc11a
Tarun Gaba <Tarun Gaba>
2013-08-07 23:38:07 +0530

    INitialized writing some tests

--- pydy_viz/static/js/spec/CanvasSpec.js
+++ pydy_viz/static/js/spec/CanvasSpec.js
@@ -1,58 +1,27 @@
-describe("Player", function() {
-  var player;
-  var song;
+describe("Canvas", function() {
+  var canvas;
+  
 
   beforeEach(function() {
-    player = new Player();
-    song = new Song();
+    // basic functionality test is to describe some standard JSONObj
+    // here and then simply check the different methods for the values
+    // from it.
+    var JSONObj = // What to do ?
+    var canvas = new Canvas(JSONObj);
+    
   });
 
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
+  it("should be able to initialize", function() {
+    canvas.initialize();
 
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
+    expect(canvas.scene.axes).toEqual(some_axes_stuff);
+    // Rest scene related attributes can be checked similarly..
+
+    expect(canvas.scene.axes).toEqual(some_axes_stuff);
+    expect(canvas.scene.axes).toEqual(some_axes_stuff);
+    expect(canvas.scene.axes).toEqual(some_axes_stuff);
+    expect(canvas.scene.axes).toEqual(some_axes_stuff);
+
   });
 
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
-    });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
-    });
-  });
-
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrow("song is already playing");
-    });
-  });
-});
+}
