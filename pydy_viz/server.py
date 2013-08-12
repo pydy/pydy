6fcd41fea62ce026c01f55b2de3a4a5b376dcbcc
Tarun Gaba <Tarun Gaba>
2013-08-12 18:35:41 +0530

    Added Socket Server

--- pydy_viz/server.py
+++ pydy_viz/server.py
@@ -1,19 +1,56 @@
-import sys
-import BaseHTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
+import socket, threading, os
+import pydy_viz
+class Server(threading.Thread):
+    def __init__(self, port):
+        threading.Thread.__init__(self)
+        self.port = port
+        host = ''
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((host, self.port))
+        self.socket.listen(5)
+
+    def parse_data(self,data):
+        static_path = os.path.dirname(pydy_viz.__file__)
+        request = data.split(' ')[1][1:]
+        file_path = os.path.join(static_path, request)
+        try:
+            send_buffer = open(file_path).read()
+        except IOError:
+            send_buffer = '404: file not found'
+        
+        return send_buffer        
+        
+    def listen_once(self):
+        conn, addr = self.socket.accept()
+        data = conn.recv(1024)
+        sent_data = self.parse_data(data)
+        conn.send(sent_data)
+        return sent_data
+        
+        
+    def run(self):
+        host = ''
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host, self.port))
+        print 'server started successfully, waiting...'
+        s.listen(1)
+        
+        while 1:
+            try:
+                self.data = conn.recv(1024)
+            except socket.error:
+                print 'lost', addr, 'waiting..'
+                s.listen(1)
+                conn, addr = s.accept()
+                print 'contact', addr, 'on', self.now()
+                continue
+
+            if not data:
+                print 'lost', addr, 'waiting..'
+                s.listen(1)
+                conn, addr = s.accept()
+            else:    
+                sent_data = self.parse_data(self.data)
+                conn.send(sent_data)
 
 
-def create_server(port=8000):
-    HandlerClass = SimpleHTTPRequestHandler
-    ServerClass  = BaseHTTPServer.HTTPServer
-    Protocol     = "HTTP/1.0"
-
-    server_address = ('127.0.0.1', port)
-
-    HandlerClass.protocol_version = Protocol
-    httpd = ServerClass(server_address, HandlerClass)
-
-    sa = httpd.socket.getsockname()
-    print 'Your server is running on http://127.0.0.1:8000/ \n \
-           Visit in browser to see your visulization in all its glory'
-    httpd.serve_forever()
