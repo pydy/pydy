bf9c41b13cef3f2ba4ebc3b77d182816103e4b81
Tarun Gaba <Tarun Gaba>
2013-09-11 23:22:50 +0530

    Added a Default PointLight

--- pydy_viz/scene.py
+++ pydy_viz/scene.py
@@ -14,7 +14,6 @@ try:
 except ImportError:
     pass
 
-#TODOS: implement display() methods
 
 class Scene(object):
     """
@@ -108,15 +107,19 @@ class Scene(object):
         try:
             self.cameras = kwargs['cameras']
         except KeyError:
-            self.cameras = [PerspectiveCamera(self._reference_frame, \
+            self.cameras = [PerspectiveCamera('DefaultCamera', \
+                                         self._reference_frame, \
                                     self._origin.locatenew('p_camera', \
                                          10*self._reference_frame.z))]
 
         try:
             self.lights = kwargs['lights']
         except KeyError:
-            #TODO lights
-            self.lights = []
+            self.lights = [PointLight('DefaultLight', \
+                                            self._reference_frame, \
+                                self._origin.locatenew('p_light', \
+                                           10*self._reference_frame.z))]
+
 
     @property
     def name(self):
