f7d9cef60d4b54c9a002925268f24870cb1f307c
Tarun Gaba <Tarun Gaba>
2013-09-14 03:42:42 +0530

    Added some tests for class and initialization

---
+++ pydy_viz/static/js/specs/canvasSpecs.js
@@ -0,0 +1,108 @@
+describe("Canvas should initially have ", function() {
+    
+  
+    it("Reset Camera button ", function() {
+        expect($("#resetControls")).toBeDefined(); });    
+        
+    it("Start Animation button ", function() {
+        expect($("#startAnimation")).toBeDefined(); });    
+
+    it("Pause Animation button", function() {
+        expect($("#pauseAnimation")).toBeDefined(); });    
+        
+    it("Stop Animation button", function() {
+        expect($("#stopAnimation")).toBeDefined(); });                        
+
+    it("Switch Camera button", function() {
+        expect($("#switchCamera")).toBeDefined(); });                                
+        
+    it("\"Go To Frame\" Div ", function() {
+        expect($("#gotoFrame")).toBeDefined(); });
+
+});
+
+
+describe("After initialization of class, with a test JSON Object, \
+                      Canvas should have ", function() {
+    
+    var canvas;
+    
+    beforeEach(function() {
+        canvas = new Canvas(JSONObj);
+    });
+    
+    it("Time steps equal to ten ", function() {
+        expect(canvas.timeSteps).toEqual(10); });                                
+
+    it("canvas.frames to be a Object3D ", function() {
+        expect(canvas.frames instanceof 
+                           THREE.Object3D).toBeTruthy(); });                                
+        
+    it("...And canvas.frames to be named \"frames\" ", function() {
+        expect(canvas.frames.name).toEqual("frames"); });                                    
+    
+    it("canvas.grid to be a Object3D ", function() {
+        expect(canvas.grid instanceof 
+                           THREE.Object3D).toBeTruthy(); });                                
+        
+    it("...And canvas.grid to be named \"Grid\" ", function() {
+        expect(canvas.grid.name).toEqual("Grid"); });    
+    
+    it("canvas.cameras to be a Object3D ", function() {
+        expect(canvas.cameras instanceof 
+                           THREE.Object3D).toBeTruthy(); });                                
+        
+    it("...And canvas.cameras to be named \"cameras\" ", function() {
+        expect(canvas.cameras.name).toEqual("Cameras"); });    
+    
+    it("canvas.cameraPoints to be a Object3D ", function() {
+        expect(canvas.cameraPoints instanceof 
+                           THREE.Object3D).toBeTruthy(); });                                
+        
+    it("...And canvas.cameraPoints to be named \"Camera Points\" ", function() {
+        expect(canvas.cameraPoints.name).toEqual("Camera Points"); });        
+    
+    it("canvas.lights to be a Object3D ", function() {
+        expect(canvas.lights instanceof 
+                           THREE.Object3D).toBeTruthy(); });                                
+        
+    it("...And canvas.lights to be named \"Lights\" ", function() {
+        expect(canvas.lights.name).toEqual("Lights"); });    
+    
+    it("canvas.lightPoints to be a Object3D ", function() {
+        expect(canvas.lightPoints instanceof 
+                           THREE.Object3D).toBeTruthy(); });                                
+        
+    it("...And canvas.lightPoints to be named \"Light Points\" ", function() {
+        expect(canvas.lightPoints.name).toEqual("Light Points"); });            
+    
+    it("canvas.scene to be an instance of THREE.Scene ", function() {
+        expect(canvas.scene instanceof THREE.Scene).toBeTruthy(); });           
+        
+});    
+
+
+describe("During call to canvas.initialize function \
+                      Canvas should have ", function() {
+    
+    var canvas;
+    
+    beforeEach(function() {
+        canvas = new Canvas(JSONObj);
+        canvas.initialize();
+    });
+
+    it("a THREE.WebGLRenderer defined  ", function() {
+        expect(canvas.renderer instanceof 
+                        THREE.WebGLRenderer).toBeTruthy(); });
+                        
+    it("...With height and width equal to as specified in JSON Object", function() {
+        
+        expect(canvas.renderer.domElement.height).toEqual(JSONObj.height);
+        expect(canvas.renderer.domElement.width).toEqual(JSONObj.width);
+         });  
+        
+    it("canvas.container to be a canvas div", function() {
+        expect(canvas.container).toEqual($("#container")); });                        
+    
+});
