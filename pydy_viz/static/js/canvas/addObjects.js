db9fb32d6feab0b6f0611cf8659af415da9ca466
Tarun Gaba <Tarun Gaba>
2013-09-15 00:59:51 +0530

    Added Shapes for addFrame

--- pydy_viz/static/js/canvas/addObjects.js
+++ pydy_viz/static/js/canvas/addObjects.js
@@ -91,34 +91,100 @@ Canvas.prototype.addFrames = function(){
         var _frame = JSONObj.frames[key];
         console.log(_frame);
         console.log(_frame.shape);
-        switch(_frame.shape.type){
-        
-        case "Cylinder":
-            var _color = new THREE.Color().setRGB(_frame.shape.color[0],
-                           _frame.shape.color[1], _frame.shape.color[2])
+        var _color = new THREE.Color().setRGB(_frame.shape.color[0],
+                           _frame.shape.color[1], _frame.shape.color[2]);
                                     
-            var _material = new THREE.MeshLambertMaterial({
+        var _material = new THREE.MeshLambertMaterial({
                                     color:        _frame.shape.color,
                                     wireframe:          true,
                                     wireframeLinewidth: 0.1,
                                     opacity: 0.5
-                                                     })
-                                                                    
+                                                     });        
+
+        switch(_frame.shape.type){
+
+        case "Cube":
+            alert("In Cube");
+            var _geometry = new THREE.CubeGeometry(
+                                      _frame.shape.length,
+                                      _frame.shape.length,
+                                      _frame.shape.length,                                                                            
+                                      50, 50, 50);          
+            break;                          
+
+        case "Cylinder":
             var _geometry = new THREE.CylinderGeometry(
                                       _frame.shape.radius,
-                                      _frame.shape.radius,
+                                            _frame.shape.radius,
                                       _frame.shape.height,
                                       50,50);
-            var _mesh = new THREE.Mesh(_geometry, _material);
+            break;
+                                      
+        case "Cone":        
+            var _geometry = new THREE.CylinderGeometry(
+                                      _frame.shape.radius,
+                                      0.1,
+                                      _frame.shape.length,
+                                      50,50);        
+            break;
+       
+        case "Sphere":        
+            var _geometry = new THREE.SphereGeometry(
+                                      _frame.shape.radius,
+                                      100,100);
+            break;
+
+        case "Circle":        
+            var _geometry = new THREE.SphereGeometry(
+                                      _frame.shape.radius,
+                                      8);
+            break;            
+           
+        case "Plane":        
+            var _geometry = new THREE.PlaneGeometry(
+                                      _frame.shape.length,
+                                      _frame.shape.width,                                      
+                                      100);
+            break;                        
             
-            var _element = new Float32Array(_frame.simulation_matrix[0]);
-            var initMatrix = new THREE.Matrix4();
-            initMatrix.elements = _element;
-            _mesh.matrix.identity();
-            _mesh.applyMatrix(initMatrix);
-            // case for more shapes ...
-        
+        case "Tetrahedron":
+            var _geometry = new THREE.TetrahedronGeometry(
+                                      _frame.shape.radius);
+            break;                                    
+            
+        case "Octahedron":
+            var _geometry = new THREE.OctahedronGeometry(
+                                      _frame.shape.radius);
+            break;                                    
+                        
+        case "Icosahedron":
+            var _geometry = new THREE.IcosahedronGeometry(
+                                      _frame.shape.radius);
+            break;                                                
+            
+        case "Torus":
+            var _geometry = new THREE.TorusGeometry(
+                                      _frame.shape.radius,
+                                      _frame.shape.tube_radius,100
+                                      );
+            break;                                                            
+            
+        case "TorusKnot":
+            var _geometry = new THREE.TorusKnotGeometry(
+                                      _frame.shape.radius,
+                                      _frame.shape.tube_radius,100
+                                      );
+            break;                                                                        
+            
+        //. ......
         }
+
+        var _mesh = new THREE.Mesh(_geometry, _material);
+        var _element = new Float32Array(_frame.simulation_matrix[0]);
+        var initMatrix = new THREE.Matrix4();
+        initMatrix.elements = _element;
+        _mesh.matrix.identity();
+        _mesh.applyMatrix(initMatrix);
         this.frames.add(_mesh);
         
        
