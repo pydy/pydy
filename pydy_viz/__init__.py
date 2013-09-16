18be18ff5fb7e79324edd2d2b96d70a9b8ba6920
Tarun Gaba <Tarun Gaba>
2013-09-17 02:03:39 +0530

    Added version info in __init__

--- pydy_viz/__init__.py
+++ pydy_viz/__init__.py
@@ -11,6 +11,8 @@ __all__ = []
 #    determine which names are imported when
 #    "from sympy.physics.mechanics import *" is done.
 
+__version__ = "0.1.0-dev"
+
 import visualization_frame
 from visualization_frame import *
 __all__.extend(visualization_frame.__all__)
