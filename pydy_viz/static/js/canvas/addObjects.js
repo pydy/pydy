6bee55744ec6a56f80513823ef44e54d7f44a1f0
Tarun Gaba <Tarun Gaba>
2013-09-18 22:52:34 +0530

    Removed alerts, and modified Cone for proportionality

--- pydy_viz/static/js/canvas/addObjects.js
+++ pydy_viz/static/js/canvas/addObjects.js
@@ -102,7 +102,6 @@ Canvas.prototype.addFrames = function(){
         switch(_frame.shape.type){
 
         case "Cube":
-            alert("In Cube");
             var _geometry = new THREE.CubeGeometry(
                                       _frame.shape.length,
                                       _frame.shape.length,
@@ -114,14 +113,14 @@ Canvas.prototype.addFrames = function(){
             var _geometry = new THREE.CylinderGeometry(
                                       _frame.shape.radius,
                                             _frame.shape.radius,
-                                      _frame.shape.height,
+                                      _frame.shape.length,
                                       50,50);
             break;
                                       
         case "Cone":        
             var _geometry = new THREE.CylinderGeometry(
                                       _frame.shape.radius,
-                                      0.1,
+                                      _frame.shape.radius/100,
                                       _frame.shape.length,
                                       50,50);        
             break;
