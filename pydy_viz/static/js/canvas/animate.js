f1c35dbb507d0eabd881c623ebec534c41ed77b9
Tarun Gaba <Tarun Gaba>
2013-09-19 17:49:56 +0530

    bug 71 fixed.

--- pydy_viz/static/js/canvas/animate.js
+++ pydy_viz/static/js/canvas/animate.js
@@ -1,5 +1,6 @@
 
 Canvas.prototype.startAnimation = function(){
+    $("#startAnimation").unbind("click");
         
     for(var key in JSONObj.frames){
       Canvas.prototype._animate(key, Canvas.prototype.animationCounter);
@@ -14,18 +15,15 @@ Canvas.prototype.startAnimation = function(){
           cancelAnimationFrame(Canvas.prototype.animationID);          
           Canvas.prototype.animationCounter = 0;  
           }         
-  
+
 };
 
 Canvas.prototype._animate = function(key, counter)
       {
-          console.log(Canvas.prototype.scene.children[4].children[key]);
           var _element = JSONObj.frames[key].simulation_matrix[counter];
           var matrix = new THREE.Matrix4();
           matrix.elements = _element;
-          // TODO Replace exact number by getObjectbyName ..
-          console.log("key, counter, element:" + key, counter, _element);
-          
+
           Canvas.prototype.scene.getObjectByName("frames").children[key].matrix.identity();
           Canvas.prototype.scene.getObjectByName("frames").children[key].applyMatrix(matrix)
           
@@ -35,7 +33,8 @@ Canvas.prototype._animate = function(key, counter)
 Canvas.prototype.pauseAnimation = function(){
     cancelAnimationFrame(Canvas.prototype.animationID);
     Canvas.prototype.animationID = undefined;
-
+    $("#startAnimation").click(Canvas.prototype.startAnimation);  
+        
 };
 Canvas.prototype.stopAnimation = function(){
     Canvas.prototype.animationCounter = 0;
@@ -45,7 +44,8 @@ Canvas.prototype.stopAnimation = function(){
     for(var key in JSONObj.frames){
           Canvas.prototype._animate(key, 0);
     }
-                
+    $("#startAnimation").click(Canvas.prototype.startAnimation);  
+                    
 };
 
 Canvas.prototype._transform = function(key, i){
