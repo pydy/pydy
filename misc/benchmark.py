1459d740fee9a0923d1a424b9ce83904bbfbe4c8
Jason K. Moore <moorepants@gmail.com>
2013-10-29 18:29:22 -0400

    Benchmark runs with all three backends.

--- misc/benchmark.py
+++ misc/benchmark.py
@@ -1,36 +1,92 @@
-import time
-from scipy.integrate import odeint
-from numpy import hstack, ones, pi, linspace
+#!/usr/bin/env python
 
-from benchmark_functions import generate_equations, numeric_right_hand_side
+# standard library
+import time
+import os
+import shutil
+import glob
+
+# external libraries
+from scipy.integrate import odeint
+from numpy import hstack, ones, pi, linspace, array
+import matplotlib.pyplot as plt
+from pydy_code_gen.code import numeric_right_hand_side
+from pydy_code_gen.tests.models import \
+    generate_n_link_pendulum_on_cart_equations_of_motion
+
+methods = ['lambdify', 'theano', 'cython']
+
+derivation_times = {}
+integration_times = {method: {} for method in methods}
+code_generation_times = {method: {} for method in methods}
 
 for n in range(1, 11):
 
-    print("n = {}".format(n))
+    title = "Pendulum with {} links.".format(n)
+    print(title)
+    print('=' * len(title))
 
     start = time.time()
-    kane, parameters = generate_equations(n)
-    print('The derivation took {} seconds.'.format(time.time() - start))
+    results = \
+        generate_n_link_pendulum_on_cart_equations_of_motion(n,
+                                                             cart_force=False)
+    derivation_times[n] = time.time() - start
+    print('The derivation took {} seconds.\n'.format(derivation_times[n]))
 
-    # Define the numerical values: paramters, time, and initial conditions
+    # Define the numerical values: parameters, time, and initial conditions
     arm_length = 1. / n
     bob_mass = 0.01 / n
     parameter_vals = [9.81, 0.01 / n]
     for i in range(n):
         parameter_vals += [arm_length, bob_mass]
-    x0 = hstack(( 0, pi / 2 * ones(len(kane._q) - 1), 1e-3 * ones(len(kane._u)) ))
+    x0 = hstack((0, pi / 2 * ones(len(results[3]) - 1), 1e-3 *
+                 ones(len(results[4]))))
+    args = {'constants': array(parameter_vals),
+            'num_coordinates': n + 1}
     t = linspace(0, 10, 1000)
 
-    methods = ['lambdify', 'theano'] #, 'autowrap']
     for method in methods:
-        print("Running with {} method.".format(method))
-        # TODO : time the code generation part
-        # TODO : it'd also be interesting to know the time it takes to call solve
-        # as solve and the integration can be pushed to the faster languages too.
-        # We need to have a way to seperate that if the odes are pass in before the
-        # matrix is solved.
-        right_hand_side = numeric_right_hand_side(kane, parameters, generator=method)
-        print('Starting odeint')
+
+        subtitle = "Generating with {} method.".format(method)
+        print(subtitle)
+        print('-' * len(subtitle))
         start = time.time()
-        y = odeint(right_hand_side, x0, t, args=(parameter_vals,))
-        print('ODE integration took {} seconds.'.format(time.time() - start))
+        right_hand_side = numeric_right_hand_side(*results, generator=method)
+        code_generation_times[method][n] = time.time() - start
+        print('The code generation took {} seconds.'.format(
+            code_generation_times[method][n]))
+
+        start = time.time()
+        y = odeint(right_hand_side, x0, t, args=(args,))
+        integration_times[method][n] = time.time() - start
+        print('ODE integration took {} seconds.\n'.format(
+            integration_times[method][n]))
+
+# clean up the cython crud
+files = glob.glob('multibody_system*')
+for f in files:
+    os.remove(f)
+shutil.rmtree('bulid')
+
+# plot the results
+fig, ax = plt.subplots(3, 1)
+
+ax[0].plot(derivation_times.keys(), derivation_times.values(),
+           label='Symbolic Derivation')
+ax[0].set_title('Symbolic Derivation Time')
+
+for k, v in code_generation_times.items():
+    ax[1].plot(v.keys(), v.values(), label=k)
+ax[1].set_title('Code Generation Time')
+ax[1].legend()
+
+for k, v in integration_times.items():
+    ax[2].plot(v.keys(), v.values(), label=k)
+ax[2].set_title('Integration Time')
+ax[2].legend()
+
+for a in ax.flatten():
+    a.set_xlabel('Number of links.')
+    a.set_ylabel('Time [s]')
+
+fig.savefig('benchmark-results.png')
