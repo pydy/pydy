18b33f0894f3bfb888adcb5aac6ddafac19a8867
Jason K. Moore <moorepants@gmail.com>
2014-02-22 09:05:01 -0500

    Added sympy to mock.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ if os.environ.get('READTHEDOCS', None) == 'True':
     import mock
 
     MOCK_MODULES = ['numpy', 'numpy.testing', 'matplotlib',
-                    'matplotlib.colors']
+                    'matplotlib.colors', 'sympy', 'sympy.physics.mechanics',
+                    'sympy.matrices.expressions']
     for mod_name in MOCK_MODULES:
         sys.modules[mod_name] = mock.Mock()
 
