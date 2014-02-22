8031c0f8006242fa7cbf71e279be6549eab12896
Jason K. Moore <moorepants@gmail.com>
2014-02-22 08:45:37 -0500

    Added mock to docs/conf.py to allow building on readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,16 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import pydy_viz
 
+# This allows readthedocs to use autodoc.
+if os.environ.get('READTHEDOCS', None) == 'True':
+
+    import mock
+
+    MOCK_MODULES = ['numpy', 'numpy.testing', 'matplotlib',
+                    'matplotlib.colors']
+    for mod_name in MOCK_MODULES:
+        sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
