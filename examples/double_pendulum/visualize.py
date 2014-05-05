27ebd36b0ae9c52ef22080d13f4fb702a8193f6a
Christopher Dembia <cld72@cornell.edu>
2014-05-04 21:30:59 -0700

    Add explanation of viz calls.

--- examples/double_pendulum/visualize.py
+++ examples/double_pendulum/visualize.py
@@ -32,12 +32,15 @@ from simulate import *
 link = Cylinder(name='link', radius=0.5, length=constants[l], color='red')
 sphere = Sphere(name='sphere', radius=1.0)
 
-# TODO notes.
+# By default, Cylinders are drawn so that their center is at the origin of the
+# VisualizationFrame, and their axis is the y axis of the VisualizationFrame.
+# We want the end of the Cylinder to be at the origin of the
+# VisualizationFrame, and we want the Cylinder's axis to be aligned with the x
+# axis of the VisualizationFrame. For these reasons, we must use the 'orientnew' and 'locatenew' methods to create new frames/points.
 linkP_viz_frame = VisualizationFrame('linkP',
         A.orientnew('frameP', 'Axis', [0.5 * pi, N.z]),
         O.locatenew('originP', 0.5 * l * A.x),
         link)
-# TODO notes.
 linkR_viz_frame = VisualizationFrame('linkR',
         B.orientnew('frameR', 'Axis', [0.5 * pi, N.z]),
         P.locatenew('originP', 0.5 * l * B.x),
@@ -50,8 +53,13 @@ sphereR_viz_frame = VisualizationFrame('sphereR', N, R, sphere)
 # Construct the scene
 # ===================
 
-# TODO world frame...
-scene = Scene(N.orientnew('world', 'Axis', [0.5 * pi, N.z]), O,
+# We want gravity to be directed downwards in the visualization. Gravity is in
+# the -x direction. By default, the visualization uses the xz plane as the
+# ground plane. Thus, gravity is contained in the ground plane. However, we
+# want gravity to point in the -y direction in the visualization. To achieve
+# this, we create a world frame that is rotated +90 degrees about the N frame's
+# z direction.
+acene = Scene(N.orientnew('world', 'Axis', [0.5 * pi, N.z]), O,
         linkP_viz_frame, linkR_viz_frame, sphereP_viz_frame, sphereR_viz_frame)
 
 
