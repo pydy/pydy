1cb017ea5c3a0968cdf78c29933e2c25a798c179
Tarun Gaba <tarun.gaba7@gmail.com>
2014-06-23 22:56:33 +0530

    Cleaning code: added docstrings to dv.js

--- pydy/viz/static/js/dyviz/dv.js
+++ pydy/viz/static/js/dyviz/dv.js
@@ -5,48 +5,31 @@ var DynamicsVisualizer = {};
 DynamicsVisualizer = Class.create({
 	/** 
 	  * DV is the main class for Dynamics Visualizer.
-	  * 
+	  * it contains methods to set up a default UI, and 
+	  * maps buttons' `onClick` to functions.
 	**/ 
 
 	init: function(){    
 		/**
-		  * Initializes the Class with 
-		  * relevant objects.
-		  *
+		  * Checks whether the browser supports webGLs, and
+		  * initializes the DynamicVisualizer object.
 		**/
-         
 		var self = this;
 		console.log("INFO: initializing Visualizer");
-		
-
-		this._model = {}; 
-        
 		if(!self.isWebGLCompatible()){
 			console.log("ALERT: Incompatible browser!");
 			alert("The browser you are using is not compatible! " + 
 				"Please use a latest version of Chrome or Firefox");
-
 		    return false;
 		}
-		// Load different css for Browser and IPython notebook...
-		/*try{
-			jQuery('head').append('<link rel="stylesheet" type="text/css" href="css/ipython_main.css">');
-		}
-		catch(err){
-			alert("IPython notebook not triggered!")
-			console.log("IPython notebook not triggered, loading main.css");
-			jQuery('head').append('<link rel="stylesheet" type="text/css" href="css/main.css">');
-		}*/
-
-    },
+	},
 
 
 	isWebGLCompatible: function(){ 
 		/**
 		  * Checks whether the browser used is
 		  * compatible for handling webGL based
-		  * animations. Raises an alert if not!
-		  * 
+		  * animations.
 		  * Requires external script: Modernizr.js
 		  *
 		**/
@@ -69,18 +52,9 @@ DynamicsVisualizer = Class.create({
 
         var self = this;
 		jQuery("#simulation-load").click(function(){
-			// Fetch FilePath from input..
-			// load it into the canvas
-
-            self.sceneFilePath = jQuery("#json-input").val();
-            console.log("INFO: found scene JSON file:" + self.sceneFilePath);
+			self.sceneFilePath = jQuery("#json-input").val();
+            console.log("INFO: Loading scene JSON file:" + self.sceneFilePath);
 			self.Parser.loadScene();
-
-		});
-
-		jQuery("#json-save").click(function(){
-			// Activate CodeMirror... 
-			//
 		});
 
 		self._slider = jQuery("#timeSlider").slider({min:0,max:100,step:1, handle:"square", value:0});
@@ -90,22 +64,15 @@ DynamicsVisualizer = Class.create({
         	var i = 0;
         	var gotValue = false;
         	for( i=0;i<self._timeArray.length && !gotValue;i++){
-        		
         		var percent = (self._timeArray[i]/self._timeArray[len-1])*100;
-        		if(val <= percent){
-        			gotValue = true;
-        			break;
-        		}
-        	//
+        		if(val <= percent){ gotValue = true; break; }
             }
             self.Scene.setAnimationTime(self._timeArray[i]);
-
         });
 			
 			
 		jQuery("#resetControls").click(function(){
-			// Activate CodeMirror... 
-			//
+			self.scene._resetControls();
 		});
 
 		jQuery("#playAnimation").click(function(){
@@ -140,39 +107,39 @@ DynamicsVisualizer = Class.create({
 	},
 
 	loadUIElements: function(){
+		/**
+		  * This method loads UI elements
+		  * which can be loaded only **after**
+		  * scene JSON is loaded onto canvas.
+		**/
 		var self = this;
         
         jQuery("#playAnimation").removeClass("disabled");
         jQuery("#show-model").removeClass("disabled");
         var objs = self.model.objects;
-        // clear dropdown of some old objects..
-        jQuery("#objectDropdown").find("li").remove()
+  
+        jQuery("#objectDropdown").find("li").remove() // clean old dropdown list.
         
         for(var obj in objs){
-
-            var toAppend = '<li><a id="'+ objs[obj].simulation_id + 
+        	var toAppend = '<li><a id="'+ objs[obj].simulation_id + 
                            '" href="#">' + objs[obj].name + '</a></li>';
-
             jQuery("#objectDropdown").append(toAppend);
-
             // adding click functions to all dropdown objs.
-
             jQuery("#" + objs[obj].simulation_id).click(function(){
             	self.ParamEditor.openDialog(jQuery(this).attr("id"));
-          
             });
-
         }
 
         var constants = self.model.constant_map;
-        
         var div = jQuery("#simulation-params").fadeOut();
         div.html(" "); // clear html first
+        
         for(var i in constants){
         	div.append('<span class="input-group-addon">' + i + '</span>');
         	div.append(jQuery('<input />',{ type:'text', id: i, class: 'form-control', value: constants[i]}));
         }
         div.fadeIn();
+
         // enable CodeMirror...
         jQuery("#model-loader").html(" ");
         if(!self.editor){
@@ -188,6 +155,10 @@ DynamicsVisualizer = Class.create({
 
     
     getBasePath: function(){
+    	/**
+    	  * Returns the base path of
+    	  * the loaded Scene file.
+    	**/ 
     	var self = this;
 
     	var slashes_fixed = self.sceneFilePath.replace(/\\/g, "/");
@@ -195,13 +166,14 @@ DynamicsVisualizer = Class.create({
     },
 
     getFileExtenstion: function(){
+    	/**
+    	  * Returns the extension of
+    	  * the loaded Scene file.
+    	**/ 
     	var self = this;
     	return self.sceneFilePath.split(".").slice(-1)[0].toLowerCase();
 
    }
-
-
-
 });
 
 var DynamicsVisualizer = new DynamicsVisualizer();
