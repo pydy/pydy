d206014958556b64808bf2a8fc62e77682a15271
Tarun Gaba <tarun.gaba7@gmail.com>
2014-08-21 19:12:01 +0530

    Improved docs formatting

--- pydy/viz/scene.py
+++ pydy/viz/scene.py
@@ -349,7 +349,7 @@ class Scene(object):
 
         Parameters
         ----------
-        system: object, pydy.System
+        system : object, pydy.System
         
         Keyword arguments are same as generate_visualization_json.
    
