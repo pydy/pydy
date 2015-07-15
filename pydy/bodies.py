bf9879a88fb2b9179fb2e763d820575b81db2205
Sahil Shekhawat <sahilshekhawat01@gmail.com>
2015-07-15 19:55:41 +0530

    completed basic implementations of Body class

--- pydy/bodies.py
+++ pydy/bodies.py
@@ -1,19 +1,30 @@
-from sympy.physics.mechanics import RigidBody, Particle
-
-from .force import Force
+from sympy import Symbol, oo
+from sympy.physics.mechanics import RigidBody, Particle, ReferenceFrame, outer
+from sympy.physics.vector import Point
 
 __all__ = ['Body', 'Ground']
 
 
 class Body(object):
     """
+    A Body which can be connected by joints.
+
+    It can create a Particle or RigidBody depending upon the arguments passed.
+    If nothing is passed, then by default, a RigidBody is created. User can
+    also pass masscenter, mass, frame and inertia to change the arguments
+    passed to RigidBody.
+    To create a Particle only mass and masscenter needs to be passed. User can
+    also pass a frame which will be attached to the Particle.
+
+    In short, it creates a Rigidbody if inertia is passed or nothing is passed.
+    and a Particle if inertia is not passed and things like masscenter and mass
+    are.
 
     Parameters
     ---------
     name: string
-        defines the name of the body. It is used as the base for defining body specific
-        properties.
-    (Rest are same as RigidBody but optional)
+        defines the name of the body. It is used as the base for defining body
+        specific properties.
     masscenter : Point (optional)
         The point which represents the center of mass of the rigid body.
     frame : ReferenceFrame (optional)
@@ -23,51 +34,135 @@ class Body(object):
     inertia : (Dyadic, Point) (optional)
         The body's inertia about a point; stored in a tuple as shown above.
 
+    Example:
+    --------
+    1. Default behaviour. It creates a RigidBody after defining mass,
+     masscenter, frame and inertia.
+
+    >>> from pydy.bodies import Body
+    >>> body = Body('name_of_body')
+
+    2. Passing attributes of Rigidbody. All the arguments needed to create a
+     RigidBody can be passed while creating a Body too.
+
+    >>> from sympy import Symbol
+    >>> from sympy.physics.mechanics import ReferenceFrame, Point
+    >>> from pydy.bodies import Body
+    >>> mass = Symbol('mass')
+    >>> masscenter = Point('masscenter')
+    >>> frame = ReferenceFrame('frame')
+    >>> I = outer (A.x, A.x)
+    >>> inertia_tuple = (I, P)
+    >>> body = Body('name_of_body', masscenter, mass, frame, inertia_tuple)
+
+    3. Creating a Particle. If masscenter and mass are passed, and inertia is
+     not then a Particle is created.
+
+    >>> from sympy import Symbol
+    >>> from sympy import Point
+    >>> from pydy.bodies import Body
+    >>> mass = Symbol('mass')
+    >>> masscenter = Symbol('masscenter')
+    >>> body = Body('name_of_body', masscenter, mass)
+
+    Similarly, A frame can also be passed while creating a Particle.
+
     """
-    def __init__(self, name, masscenter=None, mass=None, frame=None, inertia=None):
+    def __init__(self, name, masscenter=None, mass=None, frame=None,
+                 inertia=None):
 
         self.name = name
         self.parent = None
         self.child = None
         self.force_list = []
-        self.body = None # TODO assign a better name.
-        self.coordinates = []
-        self.speeds = []
+        self.body = None  # TODO assign a better name.
+        self._coordinates = []
+        self._speeds = []
+        self._counter = 0
 
-        # TODO define properties for RigidBody.
-
-        # is user passes masscenter and mass then a particle is created otherwise a rigidbody.
-        # as a result a body may or may not have inertia.
-        if inertia is None and mass is not None:
-                self.body = Particle(self.name, self.masscenter, self.mass)
+        if masscenter is None:
+            self._masscenter = Point(self.name + '_masscenter')
         else:
-                self.body = RigidBody(self.name, self.masscenter, self.frame, self.mas
+            self._masscenter = masscenter
 
-    def add_force(self, force):
+        if mass is None:
+            self._mass = Symbol(self.name + '_mass')
+        else:
+            self._mass = mass
+
+        if frame is None:
+            self._frame = ReferenceFrame(self.name + '_frame')
+        else:
+            self._frame = frame
+
+        if inertia is None:
+            inertia = outer(self._frame.x, self._frame.x)  # tensor product
+            self._inertia = (inertia, self._masscenter)
+        else:
+            self._inertia = inertia
+
+        # If user passes masscenter and mass then a particle is created
+        # otherwise a rigidbody. As a result a body may or may not have inertia.
+        if inertia is None and mass is not None:
+            self.body = Particle(self.name, self._masscenter, self._mass)
+        else:
+            self.body = RigidBody(self.name, self._masscenter, self._frame,
+                                  self._mass, self._inertia)
+
+    def add_coordinate(self, coordinate):
+        self._coordinates.append(coordinate)
+
+    def add_speed(self, speed):
+        self._speeds.append(speed)
+
+    def add_force(self, point_vector, force_vector):
         """
         Adds force to the body by adding Force's instance to the force_list.
         force_list is used by system to get the tuples from the instance and
         create force_list for Kane's method.
 
-        Paramters
+        Parameters
         ---------
-        force: (point, vector) or vector
-            Adds the vector force to the point. point must have to be in the body.
-            If body is RigidBody, pass (point, vector) and if body is Particle then pass vector,
-            force is always applied to the masscenter for particle.
+        point_vector: A 3 Tuple
+            Defines an arbitary point on a body on which the force will be
+            applied. The tuple defines the x, y and z values of the point
+            w.r.t body's masscenter.
+        force_vector: A 3 Tuple
+            Defines the force vector w.r.t frame of the body. The tuple defines
+            the values of vector in x, y and z directions of body's frame.
+
+        Example
+        --------
+        To add a force of magnitude 1 in y direction on a point at distance of
+        1 in x direction. All the directions are w.r.t body's frame.
+
+        >>> body = Body('body')
+        >>> body.add_force((1,0,0), (0,1,0))
+
         """
-        # TODO
+        point_vector = self.convert_tuple_into_vector(point_vector)
+        force_vector = self.convert_tuple_into_vector(force_vector)
+        point = self._masscenter.locatenew(self.name + '_point' + self._counter, point_vector)
+        self.force_list.append((point, force_vector))
+        self._counter += 1
 
-    def add_coordinate(self, coordinate):
-        # TODO
-        self.coordinates.append(coordinate)
-
-    def add_speed(self, speed):
-        # TODO
-        self.speeds.append(speed)
+    def convert_tuple_into_vector(self, values_tuple):
+        vector = 0
+        vector += vector + values_tuple[0] * self.frame.x
+        vector += vector + values_tuple[1] * self.frame.y
+        vector += vector + values_tuple[2] * self.frame.z
+        return vector
 
 
 class Ground(Body):
+    """
+    A Ground Body
+
+    It has infinite mass and an origin.
+    """
     def __init__(self):
-        # TODO create defualt attributes for Ground.
-        super(Ground, self).__init__(*args, **kwargs)
+        name = 'ground'
+        masscenter = Point('origin')
+        mass = oo
+        # TODO Discuss if Ground needs to be RigidBody or Particle is fine?
+        super(Ground, self).__init__(name, masscenter, mass)
--- pydy/tests/test_bodies.py
+++ pydy/tests/test_bodies.py
@@ -19,54 +19,54 @@ class TestBody():
         assert self.body.name == 'Body'
         assert self.body.parent is None
         assert self.body.child is None
-        assert isinstance(self.body.masscenter, Point)
-        assert isinstance(self.body.frame,    ReferenceFrame)
-        assert isinstance(self.body.mass, Symbol)
-        assert len(self.body.inertia) == 2
-        assert isinstance(self.body.inertia[0], Dyadic)
-        assert isinstance(self.body.inertia[1], Point)
+        assert isinstance(self.body._masscenter, Point)
+        assert isinstance(self.body._frame,    ReferenceFrame)
+        assert isinstance(self.body._mass, Symbol)
+        assert len(self.body._inertia) == 2
+        assert isinstance(self.body._inertia[0], Dyadic)
+        assert isinstance(self.body._inertia[1], Point)
         assert isinstance(self.body.body, RigidBody)
 
     def test_body_properties_value(self):
         name = self.body.name
-        assert self.body.masscenter == Point(name + ' MassCenter')
-        assert self.body.frame == ReferenceFrame(name + ' ReferenceFrame')
-        assert self.body.mass == Symbol(name + ' Mass')
+        assert self.body._masscenter == Point(name + ' MassCenter')
+        assert self.body._frame == ReferenceFrame(name + ' ReferenceFrame')
+        assert self.body._mass == Symbol(name + ' Mass')
 
     def test_body_particle(self):
         point = Point('point')
         mass = Symbol('mass')
         self.particle_body = Body(masscenter=point, mass=mass)
         assert isinstance(self.particle_body.body, Particle)
-        assert self.particle_body.masscenter == point
-        assert self.particle_body.mass == mass
+        assert self.particle_body._masscenter == point
+        assert self.particle_body._mass == mass
 
     def test_body_particle_add_force(self):
-        vector = Symbol('a') * self.particle_body.frame.x
+        vector = Symbol('a') * self.particle_body._frame.x
         self.particle_body.add_force(vector)
-        assert self.particle_body.force_list == [(self.particle_body.masscenter, vector)]
+        assert self.particle_body.force_list == [(self.particle_body._masscenter, vector)]
 
     def test_body_add_force(self):
         assert isinstance(self.body.force_list, list())
         assert len(self.body.force_list) == 0
 
-        point = self.body.masscenter.locatenew('force_point', Symbol('l')*self.body.frame.x)
-        force_tuple = (point, Symbol('Fa')*self.body.frame.z)
+        point = self.body._masscenter.locatenew('force_point', Symbol('l')*self.body._frame.x)
+        force_tuple = (point, Symbol('Fa')*self.body._frame.z)
         self.body.add_force(force_tuple)
         assert len(self.body.force_list) == 1
         assert self.body.force_list[0] == [force_tuple]
 
     def test_body_coordinates(self):
-        assert self.body.coordinates == []
+        assert self.body._coordinates == []
         q1 = dynamicsymbols('q1')
         self.body.add_coordinate(q1)
-        assert q1 in self.body.coordinates
+        assert q1 in self.body._coordinates
 
     def test_body_speeds(self):
-        assert self.body.speeds == []
+        assert self.body._speeds == []
         u1 = dynamicsymbols('u1')
         self.body.add_speed(u1)
-        assert u1 in self.body.speeds
+        assert u1 in self.body._speeds
 
 
 class TestGround():
@@ -75,6 +75,6 @@ class TestGround():
 
     def test_ground_init(self):
         assert hasattr(self.ground, 'origin')
-        assert self.ground.masscenter == Point(self.ground.name + '_Origin')
+        assert self.ground._masscenter == Point(self.ground.name + '_Origin')
         assert self.ground.parent is None
-        assert self.ground.frame == ReferenceFrame(self.ground.name + '_Frame')
+        assert self.ground._frame == ReferenceFrame(self.ground.name + '_Frame')
