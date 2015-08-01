74cb54cf9833d081381a372bf656117eb842aaa2
Sahil Shekhawat <sahilshekhawat01@gmail.com>
2015-08-02 05:12:28 +0530

    added unit tests for cylindrical joint

--- examples/Joints/Ingen/cylindricaljoint.py
+++ examples/Joints/Ingen/cylindricaljoint.py
@@ -1,4 +1,3 @@
-from sympy import Symbol
 from pydy.bodies import Body
 from pydy.joints import CylindricalJoint
 from pydy.joints_method import JointsMethod
@@ -6,4 +5,8 @@ from pydy.joints_method import JointsMethod
 parent = Body('parent')
 child = Body('child')
 
-cylindrical_joint = CylindricalJoint('cylindrical_joint')
+cylindrical_joint = CylindricalJoint('cylindrical_joint',parent, child,
+                                     child_point_pos=(1, 1, 1))
+
+JM = JointsMethod([cylindrical_joint], parent)
+print JM.get_kanes()
