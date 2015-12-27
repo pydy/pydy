ce5d3bfae0520385ef82d5d98fe1077ad5ae6632
Jason K. Moore <moorepants@gmail.com>
2015-12-26 16:10:41 -0800

    Fixed the cython test to match the new template.

--- pydy/codegen/tests/test_cython_code.py
+++ pydy/codegen/tests/test_cython_code.py
@@ -120,8 +120,12 @@ def eval(
         expected_setup_py_source = """\
 #!/usr/bin/env python
 
-from distutils.core import setup
-from distutils.extension import Extension
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
 
 from Cython.Build import cythonize
 import numpy
