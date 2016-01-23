82275b54e90570b7946efa293c675be5d49576ec
Jason K. Moore <moorepants@gmail.com>
2016-01-23 09:59:34 -0800

    Added comment character and tests for wrap_and_indent.

--- pydy/utils.py
+++ pydy/utils.py
@@ -33,31 +33,57 @@ def sympy_equal_to_or_newer_than(version, installed_version=None):
     return parse_version(v) >= parse_version(version)
 
 
-def wrap_and_indent(lines, indentation=4, width=79, cont=None, commet=None):
+def wrap_and_indent(lines, indentation=4, width=79, continuation=None,
+                    comment=None):
     """Returns a single string in which the lines have been indented and
-    wrapped into a block of text."""
+    wrapped into a block of text.
 
-    if cont is None:
+    Parameters
+    ==========
+    indentation : integer
+        The number of characters to indent.
+    width : integer
+        The maximum line width.
+    continuation : string
+        The continuation characters.
+    comment : string
+        The character that designates a comment line.
+
+    """
+
+    if continuation is None:
         cont_len = 0
     else:
-        cont_len = len(cont)
+        cont_len = len(continuation)
+
+    if comment is None:
+        comment_len = 0
+    else:
+        comment_len = len(comment)
 
     # TODO : This will indent any lines that only contain a new line. Which
     # may not be preferable.
     new_lines = []
 
+    # TODO : The Octave printer has ".*" and "./" as operators and this doesn't
+    # deal with that.
     # add whitespace before and after [*/] binary operands between
     # subexpressions and input/output
     pattern = re.compile('(\w\])([*/])(\w)')
     for line in lines:
         if line != '\n':
             line = pattern.sub(lambda m: ' '.join(m.groups()), line)
-            wrapped = textwrap.wrap(line, width=width-indentation-cont_len,
+            wrapped = textwrap.wrap(line,
+                                    width=width-indentation-cont_len-comment_len,
                                     break_long_words=False)
-            if cont:
+            if continuation:
                 last = wrapped[-1]
-                wrapped = [l + cont for l in wrapped[:-1]]
+                wrapped = [l + continuation for l in wrapped[:-1]]
                 wrapped.append(last)
+
+            if comment:
+                for i, l in enumerate(wrapped[1:]):
+                    wrapped[i + 1] = comment + ' ' + l
         else:
             wrapped = [line]
         new_lines += wrapped
