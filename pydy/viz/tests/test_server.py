a562ab93ae08ba62c3034b628a64aacac2a96f0e
karandesai-96 <karandesai_96@live.com>
2016-02-02 23:17:04 +0530

    Adds python3 compatibility, removes redundant declaration
     - self.http_server declaration was not used anywhere so removed
     - boolean comparison using `is` refactored

--- pydy/viz/tests/test_server.py
+++ pydy/viz/tests/test_server.py
@@ -1,5 +1,11 @@
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from BaseHTTPServer import HTTPServer
+import sys
+
+# For python 2 and python 3 compatibility
+if sys.version_info < (3, 0):
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+else:
+    from http.server import SimpleHTTPRequestHandler
+    raw_input = input
 
 from ..server import StoppableHTTPServer
 
@@ -8,15 +14,13 @@ class TestStoppableHttpServer(object):
 
     def __init__(self):
         self.request_handler = SimpleHTTPRequestHandler
-        self.http_server = HTTPServer
-
         self.stoppable_http_server = StoppableHTTPServer(("127.0.0.1", 8080),
                                                          self.request_handler,
                                                          bind_and_activate=False)
 
     def test_stoppable_http_server(self):
         self.stoppable_http_server.server_bind()
-        assert self.stoppable_http_server.run is True
+        assert self.stoppable_http_server.run
 
         self.stoppable_http_server.stop()
-        assert self.stoppable_http_server.run is False
+        assert not self.stoppable_http_server.run
