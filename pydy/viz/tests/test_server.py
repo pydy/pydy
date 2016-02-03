ebb4985c673cb3e68e33e23e0db9a9a5c21154ca
karandesai-96 <karandesai_96@live.com>
2016-02-03 18:38:55 +0530

    Adds TestServer class to test Server.
     - The method `test_run_server` is broken and needs fix.

--- pydy/viz/tests/test_server.py
+++ pydy/viz/tests/test_server.py
@@ -1,4 +1,6 @@
 import sys
+import subprocess
+import signal
 
 # For python 2 and python 3 compatibility
 if sys.version_info < (3, 0):
@@ -7,7 +9,7 @@ else:
     from http.server import SimpleHTTPRequestHandler
     raw_input = input
 
-from ..server import StoppableHTTPServer
+from ..server import StoppableHTTPServer, Server
 
 
 class TestStoppableHttpServer(object):
@@ -24,3 +26,33 @@ class TestStoppableHttpServer(object):
 
         self.stoppable_http_server.stop()
         assert not self.stoppable_http_server.run
+
+
+class TestServer(object):
+
+    def __init__(self):
+        self.test_server = Server(
+                scene_file="js/tests/sample_data/scene_desc.json")
+
+    def test_run_server(self):
+        assert self.test_server.directory == "static/"
+        assert self.test_server.port == 8000
+
+        process = subprocess.Popen(self.test_server.run_server(),
+                                   stdin=subprocess.PIPE,
+                                   stdout=subprocess.PIPE)
+
+        final_port = 8000
+        while self.test_server._check_port(final_port):
+            final_port += 1
+
+        sa = self.test_server.httpd.socket.getsockname()
+        assert sa[0] == "127.0.0.1"
+        assert sa[1] == final_port
+        assert self.test_server.httpd.run
+
+        process.send_signal(signal.SIGINT)
+        out = process.communicate()
+        process.communicate("y")
+        process.kill()
+        assert not self.test_server.httpd.run
