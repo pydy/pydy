ec75265a6cfb1e587ff18df17a2f6cc0ba0dcff4
karandesai-96 <karandesai_96@live.com>
2016-02-05 16:24:23 +0530

    TestServer class now complete with test_run_server.

--- pydy/viz/tests/test_server.py
+++ pydy/viz/tests/test_server.py
@@ -1,5 +1,6 @@
+import io
+import os
 import sys
-import subprocess
 import signal
 
 # For python 2 and python 3 compatibility
@@ -22,38 +23,24 @@ class TestStoppableHttpServer(object):
 
     def test_stoppable_http_server(self):
         self.stoppable_http_server.server_bind()
-        assert self.stoppable_http_server.run
+        assert self.stoppable_http_server.running
 
         self.stoppable_http_server.stop()
-        assert not self.stoppable_http_server.run
+        assert not self.stoppable_http_server.running
 
 
 class TestServer(object):
 
     def __init__(self):
-        self.test_server = Server(directory="../static/",
+        directory = os.path.join(os.path.dirname(__file__), '../static')
+        self.test_server = Server(directory=directory,
                 scene_file="js/tests/sample_data/scene_desc.json")
 
     def test_run_server(self):
-        assert self.test_server.directory == "../static/"
-        assert self.test_server.port == 8000
+        self.test_server.run_server(headless=True)
+        assert self.test_server.httpd.running
 
-        proc = subprocess.Popen(self.test_server.run_server(),
-                                stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-
-        (stdout, stderr) = proc.communicate()
-        sa = self.test_server.httpd.socket.getsockname()
-        assert sa[0] == "127.0.0.1"
-        assert self.test_server.httpd.run
-
-        proc.communicate(signal.SIGINT, timeout=1)
-        (stdout, stderr) = proc.communicate()
-
-        proc.communicate("y", timeout=1)
-        (stdout, stderr) = proc.communicate()
-
-        proc.terminate()
-
-        assert not self.test_server.httpd.run
+        os.kill(os.getpid(), signal.SIGINT)
+        sys.stdin = io.StringIO(u'y')
+        assert not self.test_server.httpd.running
+        assert not self.test_server._thread.is_alive()
