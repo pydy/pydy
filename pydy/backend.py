bd57ee5e530bd4d05e508d537b84beaa6b8d96d5
Isuru Fernando <isuruf@gmail.com>
2016-11-22 17:55:55 +0530

    Ignore flake8 unused import error

--- pydy/backend.py
+++ pydy/backend.py
@@ -2,5 +2,5 @@ try:
     from sympy.core import backend as sm
     USE_SYMENGINE = sm.USE_SYMENGINE
 except:
-    import sympy as sm
+    import sympy as sm      # noqa: F401
     USE_SYMENGINE = False
