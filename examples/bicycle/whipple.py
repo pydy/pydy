Swap order of nonholonomic constraint to fix numerical benchmark comparison.
Moved the derivative of the pitch holonomic constraint between the two
side slip nonholonomic constraints and the floating point comparison to
Basu-Mandal now works to machine precision. Thanks to Timo Stienstra for
discovering this. This is still a bug, but we are addressing it in
SymPy. For now, this at least verifies this model can evaluate correctly
with floats.

This PR was opened Feb 7, 2015 and now on March 3, 2023 the comparison
finally works. What an ordeal! But still more work to do, as solving
linear systems symbolically is not reliable for floating point
calculations. The divide-by-zero issues are caused by solving the
nonholonomic constraint equations symbolically internal to KanesMethod.

examples/bicycle/whipple.py
@@ -279,9 +279,11 @@ def setup_symbolics():
 
     print('Defining nonholonomic constraints.')
 
-    nonholonomic = [fn.vel(N).dot(A['1']),
-                    fn.vel(N).dot(A['2']),
-                    fn.vel(N).dot(A['3'])]
+    nonholonomic = [
+        fn.vel(N).dot(A['1']),
+        fn.vel(N).dot(A['3']),
+        fn.vel(N).dot(A['2']),
+    ]
 
     # TODO : Move this out of the setup function.
     nh1 = fn.vel(N).dot(A['3'])
@@ -459,7 +461,7 @@ G_num = evalf_with_symengine(G, substitutions)
 udep_num = np.squeeze(np.linalg.solve(K_num, -G_num))
 for udi, ud_numi in zip(u_dep, udep_num):
     print('{:1.14f},{:1.14f}'.format(substitutions[udi], ud_numi))
-     substitutions[udi] = ud_numi
+    substitutions[udi] = ud_numi
     dynamic_substitutions[udi] = ud_numi
 
 print('Evaluating numerically with symengine')